01ff6b51b453e30967366065366cd96b77783b25
Sam <nuttall.sam@gmail.com>
2022-04-01 11:25:23 +0100

    add select production queries

--- API/db.py
+++ API/db.py
@@ -4,7 +4,71 @@ from contextlib import closing
 
 
 class Queries:
-    pass
+    get_student_ids = """
+                    SELECT id 
+                    FROM person 
+                    WHERE is_staff = false;
+                    """
+    get_student = """
+                    SELECT * 
+                    FROM person 
+                    WHERE is_staff = false AND id = ?;
+                    """
+    get_timetable = """
+                    SELECT 
+                        e.id,
+                        e.title,
+                        e.type,
+                        e.location,
+                        e.description,
+                        e.start_time,
+                        e.end_time,
+                        s.id,
+                        s.first_name,
+                        s.last_name,
+                        s.institute,
+                        s.image_url,
+                        pe.present,
+                        pe.mins_late
+                    FROM person AS p
+                    INNER JOIN person_event AS pe
+                        ON p.id = pe.person_id
+                    INNER JOIN event AS e
+                        ON pe.event_id = e.id
+                    INNER JOIN person AS s
+                        ON e.staff_id = s.id
+                    WHERE p.id = ?;
+                    """
+    get_emails = """
+                    SELECT 
+                        e.id,
+                        e.subject,
+                        e.content,
+                        e.sender_name,
+                        e.sender_email,
+                        e.sender_image_url,
+                        e.time,
+                        pe.read
+                    FROM person AS p
+                    INNER JOIN person_email AS pe
+                        ON p.id = pe.person_id
+                    INNER JOIN email AS e
+                        ON pe.email_id = e.id
+                    WHERE p.id = ?;
+                    """
+    get_reminders = """
+                    SELECT 
+                        r.id,
+                        r.title,
+                        r.time,
+                        re.user_added
+                    FROM person AS p
+                    INNER JOIN person_reminder AS re
+                        ON p.id = re.person_id
+                    INNER JOIN reminder AS r
+                        ON re.reminder_id = r.id
+                    WHERE p.id = ?;
+                    """
 
 
 class DB:
@@ -17,8 +81,7 @@ class DB:
         async with self.lock:
             with closing(sqlite3.connect(self.path)) as con:
                 with closing(con.cursor()) as cur:
-                    cur.execute(query, params)
-                    return cur.fetchall()
+                    return cur.execute(query, params).fetchall()
 
     async def execute_script(self, script: str):
         """Execute a script (multiple statements, seperated with ;) and commit it to the database."""
